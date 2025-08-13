ee23bb701409d62d43ed87f2b0ad302b9974e652
Adnan Labiadh <adnan.labiadh@debijenkorf.nl>
2025-08-13 10:00:00 +0200

    add health endpoint

--- backend/server/routes.js
+++ backend/server/routes.js
@@ -5,5 +5,5 @@ import healthRouter from './api/controllers/health/router.js';
 export default function routes(app) {
   app.use('/api/v1/examples', examplesRouter);
   app.use('/api/v1/products', productsRouter);
-  app.use('/api/health', healthRouter);
+  app.use('/api/v1/health', healthRouter);
 }
